de2f05009589d21fc698e00811db6ced3f2fb7e4
rzjuliannofficial <nabhanrizqijuliansaputro@gmail.com>
2025-06-04 10:19:23 +0700

    ada

--- Main03.java
+++ Main03.java
@@ -12,10 +12,7 @@ public class Main03 {
             System.out.println("2. Tampilkan Antrian");
             System.out.println("3. Check Jumlah Antrian");
             System.out.println("4. Layani Kendaraan");
-            System.out.println("5. Tampilkan Riwayat Transaksi");
-            System.out.println("0. Keluar");
-            System.out.println("======================================");
-            System.out.print("Masukkan Pilihan Anda: ");
+           
             pilihan = sc03.nextInt();
             System.out.println();
             switch (pilihan){
