4f8d0542267c721fe59ff2f916474df2569a195b
Muhammad Hanif <hanifmuhammad1309@gmail.com>
2025-06-04 10:24:24 +0700

    rusak

--- Main03.java
+++ Main03.java
@@ -8,9 +8,6 @@ public class Main03 {
         
         do {
             System.out.println("============= MENU SPBU  ============");
-            System.out.println("1. Tambah Antrian");
-            System.out.println("2. Tampilkan Antrian");
-            System.out.println("3. Check Jumlah Antrian");
             System.out.println("4. Layani Kendaraan");
             System.out.println("5. Tampilkan Riwayat Transaksi");
             System.out.println("0. Keluar");
